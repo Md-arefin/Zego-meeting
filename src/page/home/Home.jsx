009c0d9b6321a2b5c765ca3a34bf2c7f213ccff3
Md-arefin <ansariarefin617@gmail.com>
2023-09-01 02:12:11 +0600

    zego set up

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,37 +1,37 @@
-// import React from 'react';
-// import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-// import { useParams } from 'react-router-dom';
+import React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import { useParams } from 'react-router-dom';
 
 
-// const Home = ({name}) => {
-//     console.log(name)
-//     const { roomCode: roomId } = useParams();
-//     console.log("roomId", roomId);
+const Home = ({name}) => {
+    console.log(name)
+    const { roomCode: roomId } = useParams();
+    console.log("roomId", roomId);
 
-//     const myMeeting = async (element) => {
-//         const appID = 1185994261;
-//         const serverSecret = "1514ad20acd9e0da5c3a9ba1ac413770";
-//         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-//             appID,
-//             serverSecret,
-//             roomId,
-//             Date.now().toString(),
-//             'Rakibul Ansari',
-//         );
-//         const zp = ZegoUIKitPrebuilt.create(kitToken);
-//         zp.joinRoom({
-//             container: element,
-//             scenario: {
-//                 mode: ZegoUIKitPrebuilt.VideoConference,
-//             },
-//         });
-//     };
+    const myMeeting = async (element) => {
+        const appID = 1185994261;
+        const serverSecret = "1514ad20acd9e0da5c3a9ba1ac413770";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            roomId,
+            Date.now().toString(),
+            'Rakibul Ansari',
+        );
+        const zp = ZegoUIKitPrebuilt.create(kitToken);
+        zp.joinRoom({
+            container: element,
+            scenario: {
+                mode: ZegoUIKitPrebuilt.VideoConference,
+            },
+        });
+    };
 
-//     return (
-//         <div>
-//            <div ref={myMeeting} />
-//         </div>
-//     );
-// };
+    return (
+        <div>
+           <div ref={myMeeting} />
+        </div>
+    );
+};
 
-// export default Home;
+export default Home;
